d66e692722b369ac314635c1f06a348530bca2d5
rkhaneng <83090483+rkhaneng@users.noreply.github.com>
2022-04-23 03:14:20 +0100

    GitHub Hook

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!Learning java from AWS!" );
+        System.out.println( "Hello World!Learning java from AWS!Testing GitHub Hook" );
     }
 }
